b8c1dbccb3c046333275564fb8438c368fbae197
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-04 19:51:30 +0300

    handle-leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_node_is_leaf - a function that checks if a node is a leaf
+ * @node: a pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ * If node is NULL, return 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+	{
+		return (0);
+	}
+
+	if (node->left || node->right)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
