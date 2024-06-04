b93a13608ebe29b1b76081362c35d4ebf9033b35
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-06-04 19:29:44 +0300

    3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -16,12 +16,10 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree->left)
 	{
 		binary_tree_delete(tree->left);
-		free(tree->left);
 	}
 	if (tree->right)
 	{
 		binary_tree_delete(tree->right);
-		free(tree->right);
 	}
 
 	free(tree);
