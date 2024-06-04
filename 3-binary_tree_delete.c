17f8df0a876615fc18a97843caab5f63265667f7
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-04 19:24:40 +0300

    update-del

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_delete - function that deletes an entire binary tree
- * @tree:  is a pointer to the root node
+ * @tree: is a pointer to the root node
  * Return: nothing
  */
 
@@ -23,5 +23,6 @@ void binary_tree_delete(binary_tree_t *tree)
 		binary_tree_delete(tree->right);
 		free(tree->right);
 	}
-	return;
+
+	free(tree);
 }
